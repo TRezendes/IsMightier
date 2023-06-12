6fa95e35a1012bad8bf2b8f9a91b82f2773fc2c5
Timothy Rezendes <78620355+TRezendes@users.noreply.github.com>
2023-06-12 00:17:36 -0400

    All models now reflected in models.py

--- ismightier_app/models.py
+++ ismightier_app/models.py
@@ -1,4 +1,6 @@
 from ismightier_app import db
+from sqlalchemy.dialects import postgresql
+from sqlalchemy import PrimaryKeyConstraint
 
 db.reflect()
 
@@ -7,3 +9,59 @@ class USCongressTbl(db.Model):
     __table_args__ = {
         'autoload_with': db.engine
     }
+    
+class LetterPartTbl(db.Model):
+    __tablename__ = 'letter_part'
+    __table_args__ = {
+        'autoload_with': db.engine
+    }
+
+class RepresentativeSentimentTbl(db.Model):
+    __tablename__ = 'representative_sentiment'
+    #__table_args__ = (
+        #db.PrimaryKeyConstraint(
+        #    'full_name',
+        #    'state',
+        #    'subject'
+        #),
+    __table_args__ = {
+            'autoload':True,
+            'autoload_with': db.engine
+        }
+    #)
+    
+class SentimentLevelTbl(db.Model):
+    __tablename__ = 'letter_part'
+    __table_args__ = {
+        'autoload_with': db.engine
+    }
+    
+class FederalSponsorTbl(db.Model):
+    __tablename__ = 'federal_sponsor'
+    #__table_args__ = (
+    #    db.PrimaryKeyConstraint(
+    #        'bill',
+    #        'sponsor_bioguide_id'
+    #    ),
+    __table_args__ = {
+            'autoload':True,
+            'autoload_with': db.engine
+        }
+    #)
+
+class StateSponsorTbl(db.Model):
+    __tablename__ = 'state_sponsor'
+    #__table_args__ = (
+        #db.PrimaryKeyConstraint(
+        #    'state',
+        #    'bill',
+        #    'sponsor_name'
+        #),
+    __table_args__ = {
+            'autoload':True,
+            'autoload_with': db.engine
+        }
+    #)
+
+
+# USCongressTbl,LetterPartTbl,RepresentativeSentimentTbl,SentimentLevelTbl,FederalSponsorTbl,StateSponsorTbl
