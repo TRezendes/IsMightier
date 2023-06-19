635ff84407dfad2624574c62814e6e3068c827c4
Timothy Rezendes <78620355+TRezendes@users.noreply.github.com>
2023-06-19 16:50:07 -0400

    Improvements to letter display

--- ismightier_app/letters/letters_routes.py
+++ ismightier_app/letters/letters_routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, current_app, flash, redirect, render_template, request, session, url_for
 from ismightier_app.models import LetterPartTbl
 from wtforms.validators import ValidationError
+## WeasyPrint does not work under Windows 10. Stupid Windows. ##
+#from flask_weasyprint import HTML, render_pdf   
 from sqlalchemy import and_,create_engine,or_
 from numpy.random import randint
-import pandas as pd
-#from .letters_forms import 
+from .letters_forms import LetterOptionsForm
 from ismightier_app import db
 from . import letters
+import pandas as pd
 import requests
 import json
 
@@ -47,7 +49,28 @@ def rep_info(name):
         partDict['text']=partText
         partDict['color']=partColor + '50'
         letterDict[selector]=partDict
+    if 'Senator' in namedRep.iloc[0]['title']:
+        salutationTitle='Senator'
+    elif 'Representative' in namedRep.iloc[0]['title']:
+        salutationTitle='Representative'
+    elif 'Vice President' in namedRep.iloc[0]['title']:
+        salutationTitle='Vice President'
+    elif 'President' in namedRep.iloc[0]['title']:
+        salutationTitle='President'
+    elif 'Lieutenant Governor' in namedRep.iloc[0]['title']:
+        salutationTitle='Lieutenant Governor'
+    elif 'Governor' in namedRep.iloc[0]['title']:
+        salutationTitle='Governor'
+    elif 'Commissioner' in namedRep.iloc[0]['title']:
+        salutationTitle='Commissioner'
+    elif 'Mayor' in namedRep.iloc[0]['title']:
+        salutationTitle='Mayor'
+    else:
+        salutationTitle=None
+    salutationName=namedRep.iloc[0]['name'].split(' ')[-1]
+    lookupAddress=session['lookupAddress']
     ##################################################
+
     return render_template(
         '/letters/rep-info.html',
         namedRep=namedRep,
@@ -56,7 +79,10 @@ def rep_info(name):
         lookupState=lookupState,
         ###########
         selectors=selectors,
-        letterDict=letterDict
+        letterDict=letterDict,
+        salutationTitle=salutationTitle,
+        salutationName=salutationName,
+        lookupAddress=lookupAddress
         ###########
     )
 
@@ -87,3 +113,8 @@ def letter():
         selectors=selectors,
         letterDict=letterDict
     )
+
+@letters.route('<name>/pdf')
+def pdf_print(name):
+    html = render_template('/letters/letter.html')
+    return render_pdf(HTML(string=html))
