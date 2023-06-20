d0136d8ce689973476fa5420a6eb2b09b9cd586d
Timothy Rezendes <78620355+TRezendes@users.noreply.github.com>
2023-06-20 14:45:40 -0400

    Began moving letter construction to letters_funcs

--- ismightier_app/letters/letters_funcs.py
+++ ismightier_app/letters/letters_funcs.py
@@ -9,5 +9,41 @@ import requests
 import json
 
 
-def BuildLetter():
-    
+def BuildLetter(namedRep: pandas.core.frame.DataFrame, address: string) -> dict:
+    letterType=randint(3)
+    if letterType==0:
+        selectors=['whole']
+    elif letterType==1:
+        selectors=['intro','middle','conclusion']
+    elif letterType==2:
+        selectors=['intro','middle1','middle2','conclusion']
+    letterDict={}
+    for selector in selectors:
+        partDict={}
+        records=db.session.execute(db.select(LetterPartTbl).where(LetterPartTbl.part_placement==selector)).scalars().all()
+        numRecords=len(records)
+        randIndex=randint(numRecords)
+        partText=records[randIndex].part_text
+        partDict[selector]=partText
+        letterDict['parts']=partDict
+    if 'Senator' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='Senator'
+    elif 'Representative' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='Representative'
+    elif 'Vice President' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='Vice President'
+    elif 'President' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='President'
+    elif 'Lieutenant Governor' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='Lieutenant Governor'
+    elif 'Governor' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='Governor'
+    elif 'Commissioner' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='Commissioner'
+    elif 'Mayor' in namedRep.iloc[0]['title']:
+        letterDict['salutationTitle']='Mayor'
+    else:
+        letterDict['salutationTitle']=None
+    letterDict['salutationName']=namedRep.iloc[0]['name'].split(' ')[-1]
+    letterDict['address']=address
+    return letterDict
--- ismightier_app/letters/letters_routes.py
+++ ismightier_app/letters/letters_routes.py
@@ -4,8 +4,9 @@ from wtforms.validators import ValidationError
 ## WeasyPrint does not work under Windows 10. Stupid Windows. ##
 #from flask_weasyprint import HTML, render_pdf   
 from sqlalchemy import and_,create_engine,or_
-from numpy.random import randint
 from .letters_forms import LetterOptionsForm
+from .letters_funcs import BuildLetter
+from numpy.random import randint
 from ismightier_app import db
 from . import letters
 import pandas as pd
@@ -21,6 +22,7 @@ def rep_info(name):
     fieldList=[]
     addressList=[]
     lookupState=session['lookupState']
+    lookupAddress=session['lookupAddress']
     for col in namedRep.columns:
         try: 
             if col.split('.')[1] in ['city','state','zip','line1','line2']:
@@ -31,46 +33,15 @@ def rep_info(name):
             fieldList.append(col)
 
     ########## This section for included template. For iFrame, remove this section and use separate route. #####
-    letterType=randint(3)
-    if letterType==0:
-        selectors=['whole']
-    elif letterType==1:
-        selectors=['intro','middle','conclusion']
-    elif letterType==2:
-        selectors=['intro','middle1','middle2','conclusion']
-    letterDict={}
-    for selector in selectors:
-        partDict={}
-        records=db.session.execute(db.select(LetterPartTbl).where(LetterPartTbl.part_placement==selector)).scalars().all()
-        numRecords=len(records)
-        randIndex=randint(numRecords)
-        partText=records[randIndex].part_text
-        partColor=records[randIndex].color
-        partDict['text']=partText
-        partDict['color']=partColor + '50'
-        letterDict[selector]=partDict
-    if 'Senator' in namedRep.iloc[0]['title']:
-        salutationTitle='Senator'
-    elif 'Representative' in namedRep.iloc[0]['title']:
-        salutationTitle='Representative'
-    elif 'Vice President' in namedRep.iloc[0]['title']:
-        salutationTitle='Vice President'
-    elif 'President' in namedRep.iloc[0]['title']:
-        salutationTitle='President'
-    elif 'Lieutenant Governor' in namedRep.iloc[0]['title']:
-        salutationTitle='Lieutenant Governor'
-    elif 'Governor' in namedRep.iloc[0]['title']:
-        salutationTitle='Governor'
-    elif 'Commissioner' in namedRep.iloc[0]['title']:
-        salutationTitle='Commissioner'
-    elif 'Mayor' in namedRep.iloc[0]['title']:
-        salutationTitle='Mayor'
-    else:
-        salutationTitle=None
-    salutationName=namedRep.iloc[0]['name'].split(' ')[-1]
-    lookupAddress=session['lookupAddress']
+    letterDict=BuildLetter(namedRep,lookupAddress)
     ##################################################
-
+    if letterDict['salutationTitle']:
+        salutation=f"Dear {letterDict['salutationTitle']} {letterDict['salutationName']},"
+    else:
+        salutation=f"Dear {letterDict['salutationName']},"
+    letterDefaultText=f"""
+    Dear {salutationTitle} {}
+    """
     return render_template(
         '/letters/rep-info.html',
         namedRep=namedRep,
@@ -78,11 +49,7 @@ def rep_info(name):
         addressList=addressList,
         lookupState=lookupState,
         ###########
-        selectors=selectors,
-        letterDict=letterDict,
-        salutationTitle=salutationTitle,
-        salutationName=salutationName,
-        lookupAddress=lookupAddress
+        letterDict=letterDict
         ###########
     )
 
